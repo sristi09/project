b992fca8b93e10feb7d2b8696726888af27930cd
Sristi <sristi.singh0911@gmail.com>
2022-07-09 14:05:14 +0530

    Swiper done

--- home.php
+++ home.php
@@ -31,6 +31,48 @@
     </section>
 
     <!-- header section ends -->
+    <!-- home section starts  -->
+
+<section class="home">
+
+<div class="swiper home-slider">
+
+   <div class="swiper-wrapper">
+
+      <div class="swiper-slide slide" style="background:url(images/home-slide-1.jpg) no-repeat">
+         <div class="content">
+            <span>explore, discover, travel</span>
+            <h3>travel around the world</h3>
+            <a href="package.php" class="btn">discover more</a>
+         </div>
+      </div>
+
+      <div class="swiper-slide slide" style="background:url(images/home-slide-2.jpg) no-repeat">
+         <div class="content">
+            <span>explore, discover, travel</span>
+            <h3>discover the new places</h3>
+            <a href="package.php" class="btn">discover more</a>
+         </div>
+      </div>
+
+      <div class="swiper-slide slide" style="background:url(images/home-slide-3.jpg) no-repeat">
+         <div class="content">
+            <span>explore, discover, travel</span>
+            <h3>make your tour worthwhile</h3>
+            <a href="package.php" class="btn">discover more</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="swiper-button-next"></div>
+   <div class="swiper-button-prev"></div>
+
+</div>
+
+</section>
+
+<!-- home section ends -->
 
 
 

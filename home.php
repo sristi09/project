bc704fb84a22ae37a8b03360743e25f2c8e63a95
Sristi <sristi.singh0911@gmail.com>
2022-07-09 13:51:25 +0530

    Footer done

--- home.php
+++ home.php
@@ -50,13 +50,15 @@
     <!--footer section starts -->
 
     <section class="footer">
+
         <div class="box-container">
-            <div class="box>
-                <h3>Quick links</h3>
-                <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
-                <a href="about.php"><i class="fas fa-angle-right"></i>About Us</a>
-                <a href="package.php"><i class="fas fa-angle-right"></i>Packages</a>
-                <a href="book.php"><i class="fas fa-angle-right"></i>Book</a>
+
+            <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+            <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+            <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
             </div>
 
             <div class="box">

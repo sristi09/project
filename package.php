d5a60c7104e49ce02b78b1a799c3661c67414b06
Sristi <sristi.singh0911@gmail.com>
2022-07-18 14:14:36 +0530

    Package page started

---
+++ package.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>package</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--footer section starts -->
+
+<section class="footer">
+
+<div class="box-container">
+
+    <div class="box">
+    <h3>quick links</h3>
+    <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+    <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+    <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+    <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+    </div>
+
+    <div class="box">
+        <h3>Extra Links</h3>
+        <a href="#"><i class="fas fa-angle-right"></i>Ask Questions</a>
+        <a href="#"><i class="fas fa-angle-right"></i>About us</a>
+        <a href="#"><i class="fas fa-angle-right"></i>Privacy Policy</a>
+        <a href="#"><i class="fas fa-angle-right"></i>Terms of use</a>
+    </div>
+
+    <div class="box">
+        <h3>Contact Info</h3>
+        <a href="#"><i class="fas fa-phone"></i>987654321</a>
+        <a href="#"><i class="fas fa-phone"></i>123456789</a>
+        <a href="#"><i class="fas fa-envelope"></i>sristi.singh0911@gmail.com</a>
+        <a href="#"><i class="fas fa-map"></i>Kolkata,700099</a>
+    </div>
+
+    <div class="box">
+        <h3>Follow us</h3>
+        <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+        <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+        <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+        <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+    </div>
+
+
+</div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
